12f5311d71831f95a6dd9925e36584735097c0bc
suhaila10 <117509232+suhaila10@users.noreply.github.com>
2022-11-08 02:41:14 +0800

    Update citizenList.java

--- citizenList.java
+++ citizenList.java
@@ -6,7 +6,6 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 import java.util.StringTokenizer;
-
 import javax.swing.JOptionPane;
 
 
@@ -36,23 +35,22 @@ public class citizenList {
             }
             in.close();
            
-          //stack
+          //create stack
 		  Stack<citizen> stCenter1 = new Stack<>();
 		  Stack<citizen> stCenter2 = new Stack<>();
 		  Stack<citizen> stCenter3 = new Stack<>();
-		  Stack<String> fdStatus = new Stack<>();
-		  Stack<String> sdStatus = new Stack<>();
-		  Stack<citizen> temp = new Stack<>();	
+		  Stack<citizen> temp = new Stack<>();
+		  Stack<citizen> temp2 = new Stack<>();
+		  Stack<citizen> temp3 = new Stack<>();
 		  
-		  //queue
+		  
+		  //create queue
 		  Queue<citizen> qCenter1 = new LinkedList<>();
 		  Queue<citizen> qCenter2 = new LinkedList<>();
 		  Queue<citizen> qCenter3 = new LinkedList<>();
-		  Queue<citizen> completedList = new LinkedList<>();
-		  Queue<citizen> completedList1 = new LinkedList<>();
-		  Queue<citizen> completedList2 = new LinkedList<>();
-		  Queue<citizen> completedList3 = new LinkedList<>();
+		  Queue<citizen> completedList = new LinkedList<>();	  
 		  
+		  //
 		  for(int i=0; i<citizenList.size(); i++)
 	      {
 	        	Citizen = citizenList.get(i);
@@ -70,142 +68,144 @@ public class citizenList {
 	                stCenter3.push(Citizen);
 	            }
 	        }
+		  
+		  //welcome page
+		  JOptionPane.showMessageDialog(null,"LET'S GET YOUR VACCINE! :)","WELCOME PAGE", JOptionPane.INFORMATION_MESSAGE);
+
 		
 		//instructor  
 		int menu = 0;
 		do {
-			menu = Integer.parseInt(JOptionPane.showInputDialog("Please choose \nMENU 1 - ADD CITIZEN \nMENU 2 - REMOVE CITIZEN \nMENU 3 - FIRST DOSE STATUS \nMENU 4-SECOND DOSE STATUS"));
-            if (menu==1)
+			menu = Integer.parseInt(JOptionPane.showInputDialog("Please choose \nMENU 1 - ADD CITIZEN \nMENU 2 - REMOVE CITIZEN \nMENU 3 - FIRST DOSE STATUS \nMENU 4 - SECOND DOSE STATUS \nMENU 5 - EXIT"));
+            if (menu==1) //add new citizen
             {
-                String name = JOptionPane.showInputDialog("Please enter the citizen's name : ");
-                String IC = JOptionPane.showInputDialog("Please enter the citizen's IC : ");
-                String state = JOptionPane.showInputDialog("Please enter the citizen's state :");
-                int age = Integer.parseInt(JOptionPane.showInputDialog("Please enter the citizen's age : "));
-                String category = JOptionPane.showInputDialog("Please add the category :");
+                String name = JOptionPane.showInputDialog("Enter the citizen's name: ");
+                String IC = JOptionPane.showInputDialog("Enter the citizen's IC: ");
+                String state = JOptionPane.showInputDialog("Enter the citizen's state:");
+                int age = Integer.parseInt(JOptionPane.showInputDialog("Enter the citizen's age: "));
+                String category = JOptionPane.showInputDialog("Enter the citizen's category:");
                 String firstdose = null;
                 String secdose = null;
                 String vaccompletion = null;
 
                 Citizen = new citizen(name,IC,state,age,category,firstdose,secdose,vaccompletion);
+                //create object
                 citizenList.add(Citizen);
 
             }
-			else if (menu ==2)
+            
+            else if (menu ==2) //remove citizen
 			{
-	    		//ask user to enter serial number
-	             String name = JOptionPane.showInputDialog("Enter people's name that you want to remove");
+	    		//ask user to enter the IC's number of the people they want to delete
+	             String ic = JOptionPane.showInputDialog("Enter the IC's number that you want to remove");
 	             for (int i = 0; i<citizenList.size(); i++)
 	             {
 	                 Citizen = citizenList.get(i);//retrieve object
-	                 if (Citizen.getName().equalsIgnoreCase(name))
+	                 if (Citizen.getIc().equalsIgnoreCase(ic))
 	                 {
 	                     citizenList.remove(Citizen); //remove object
+	                     
 	                 }
 	                 
 	             } 
 	    		 
 	    	 }
-			else if (menu == 3)
+            
+			else if (menu == 3) //update first dose status
 			{
-				while (!stCenter1.isEmpty())
+				while (!stCenter1.isEmpty()) //put into stack
 				{
 					Citizen = stCenter1.pop();
-					
-					if (Citizen.getFirstdose().equalsIgnoreCase("null"))
-					{
-						Citizen.setFirstdose("complete");
-						temp.push(Citizen);
-					}
+                    Citizen.setFirstdose("complete");
+					temp.push(Citizen);
+					qCenter1.add(Citizen);
 				}
-				System.out.println(temp);
+				System.out.println("\t\t\t\t\t\t\t\t\t\t\tstCenter 1 after first dose");
+				System.out.println("\t _____________________________________________________________________________________________________________________________________________________________________________");
+                System.out.println(String.format("\t | %32s | %12s | %35s | %5s | %10s | %11s | %18s | %16s |", "Name", "IC Number", "State", "Age", "Category", "First Dose Status", "Second Dose Status", "Vaccine Certificate"));
+                System.out.println("\t _____________________________________________________________________________________________________________________________________________________________________________");
+			    System.out.println(temp); //after first dose
+			    System.out.println("\t _____________________________________________________________________________________________________________________________________________________________________________");
 				
 				while (!stCenter2.isEmpty())
 				{
 					Citizen = stCenter2.pop();
-					
-					if (Citizen.getFirstdose().equalsIgnoreCase("null"))
-					{
-						Citizen.setFirstdose("complete");
-						temp.push(Citizen);
-					}
+					Citizen.setFirstdose("complete");
+					temp2.push(Citizen);
+					qCenter2.add(Citizen);
 				}
-				System.out.println(temp);
+				System.out.println("\n\n\t\t\t\t\t\t\t\t\t\tstCenter 2 after first dose");
+				System.out.println("\t _____________________________________________________________________________________________________________________________________________________________________________");
+                System.out.println(String.format("\t | %32s | %12s | %35s | %5s | %10s | %11s | %18s | %16s |", "Name", "IC Number", "State", "Age", "Category", "First Dose Status", "Second Dose Status", "Vaccine Certificate"));
+                System.out.println("\t _____________________________________________________________________________________________________________________________________________________________________________");
+			    System.out.println(temp2); //display stCenter2
+			    System.out.println("\t _____________________________________________________________________________________________________________________________________________________________________________");
 				
 				while (!stCenter3.isEmpty())
 				{
 					Citizen = stCenter3.pop();
-					
-					if (Citizen.getFirstdose().equalsIgnoreCase("null"))
-					{
-						Citizen.setFirstdose("complete");
-						temp.push(Citizen);
-					}
+					Citizen.setFirstdose("complete");
+					temp3.push(Citizen);
+					qCenter3.add(Citizen);
 				}
-				System.out.println(temp);
+				System.out.println("\n\n\t\t\t\t\t\t\t\t\t\tstCenter 3 after first dose");
+				System.out.println("\t _____________________________________________________________________________________________________________________________________________________________________________");
+                System.out.println(String.format("\t | %32s | %12s | %35s | %5s | %10s | %11s | %18s | %16s |", "Name", "IC Number", "State", "Age", "Category", "First Dose Status", "Second Dose Status", "Vaccine Certificate"));
+                System.out.println("\t _____________________________________________________________________________________________________________________________________________________________________________");
+			    System.out.println(temp3); //display stCenter3
+			    System.out.println("\t _____________________________________________________________________________________________________________________________________________________________________________");
 			}
-			else if (menu ==4)
+            
+			else if (menu ==4) //update second dose status
 			{
 				int submenu = 0;
 				do
 				{
 					submenu = Integer.parseInt(JOptionPane.showInputDialog("MENU 1 - UPDATE SECOND DOSE VACCINATION STATUS \nMENU 2 - DISPLAY ALL \nMENU 3 - EXIT"));
 					
-					if (menu == 1) {
-					while (!qCenter1.isEmpty())
+					if (submenu == 1) {
+					while (!qCenter1.isEmpty()) //retrieve and remove element
 					{
-						Citizen = qCenter1.element();
-						
-						if (Citizen.getSeconddose().equalsIgnoreCase("null"))
-						{
-							Citizen.setSeconddose("complete");
-							Citizen.setVaccompletion("CONGRATULATION! YOU HAVE COMPLETE YOUR VACCINATION");
-							completedList1.add(Citizen);
-					    }
+						Citizen = qCenter1.remove();
+						Citizen.setSeconddose("complete");
+						Citizen.setVaccompletion("CONGRATULATION! YOU ARE VACCINATED");
+						completedList.add(Citizen);
 					}
-					completedList.add(Citizen);
 					
-					while (!qCenter2.isEmpty())
+					
+					while (!qCenter2.isEmpty()) //retrieve and remove element
 					{
-						Citizen = qCenter2.element();
-						if (Citizen.getSeconddose().equalsIgnoreCase("null"))
-						{
-							Citizen.setSeconddose("complete");
-							Citizen.setVaccompletion("CONGRATULATION! YOU HAVE COMPLETE YOUR VACCINATION");
-							completedList2.add(Citizen);
-                        }
+						Citizen = qCenter2.remove();
+						Citizen.setSeconddose("complete");
+						Citizen.setVaccompletion("CONGRATULATION! YOU ARE VACCINATED");
+						completedList.add(Citizen);
+
 					}
-					completedList.add(Citizen);
-					
-					
-					while (!qCenter3.isEmpty())
+
+					while (!qCenter3.isEmpty()) //retrieve and remove element
 					{
-						Citizen = qCenter3.element();
-						if (Citizen.getSeconddose().equalsIgnoreCase("null"))
-						{
-							Citizen.setSeconddose("complete");
-							Citizen.setVaccompletion("CONGRATULATION! YOU HAVE COMPLETE YOUR VACCINATION");
-							completedList3.add(Citizen);
-						}
+						Citizen = qCenter3.remove();
+						Citizen.setSeconddose("complete");
+						Citizen.setVaccompletion("CONGRATULATION! YOU ARE VACCINATED");
+						completedList.add(Citizen);
 					}
-					completedList.add(Citizen);
-					
 					}
 					else if (submenu ==2)
 					{
-						while(!completedList.isEmpty())
-						{
-							Citizen = completedList.remove();
-							System.out.println(Citizen.toString());
-						}
-						
-						
-						
-
+						System.out.println("\n \n \n");
+						System.out.println("\t\t\t\t\t\t\t\t\t\tSECOND DOSE IS UPDATED");
+						System.out.println("\t __________________________________________________________________________________________________________________________________________________________________________________________");
+		                System.out.println(String.format("\t | %32s | %12s | %35s | %5s | %10s | %11s | %18s | %34s |", "Name", "IC Number", "State", "Age", "Category", "First Dose Status", "Second Dose Status", "Vaccine Certificate"));
+		                System.out.println("\t ____________________________________________________________________________________________________________________________________________________________________________________________");
+					    System.out.println(completedList); //display all queue
+					    System.out.println("\t ____________________________________________________________________________________________________________________________________________________________________________________________");
 					}
 				} while (submenu != 3);
-        
-     }while ( menu != 6);
-      }
+			}
+            
+		}while ( menu != 5);
+		
+		}
 		catch (Exception ex)
 		{
 			System.out.println (ex.getMessage());
